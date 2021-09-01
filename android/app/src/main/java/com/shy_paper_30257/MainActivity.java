953813bd5954846187b51240050a035e9afa239a
Crowdbotics <team@crowdbotics.com>
2021-09-01 18:33:52 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/shy_paper_30257/MainActivity.java
@@ -0,0 +1,15 @@
+package com.shy_paper_30257;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "shy_paper_30257";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "shy_paper_30257Identifier",
+  url: "https://shy_paper_30257Identifier.botics.co",
+  api: "https://shy_paper_30257Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
